3de39e0e22f810dc5aa56c3fda0fb8eedced92ae
fabiolarodriguez1220-debug <fabiolarodriguez1220@gmail.com>
2025-12-16 19:42:54 -0300

    Update home route response and run configuration

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return ""
-if __name__ == '_main_':
-    app.run()
+    return "Hola, examen de Programación para Internet"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
